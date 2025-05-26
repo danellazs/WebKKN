c5bf27c7eb2e5e6f6035e3c81b0e463130745162
danellazs <danella.zefanya@gmail.com>
2025-05-26 21:17:50 +0700

    small fixes for vercel

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,6 +1,9 @@
+console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
