18fbba37b166d892eb33fb20e02422205cb9cf25
danellazs <danella.zefanya@gmail.com>
2025-05-27 20:38:01 +0700

    story into context

---
+++ src/context/story.tsx
@@ -0,0 +1,13 @@
+// src/types/story.ts
+
+export interface Story {
+  id: number;
+  latitude: number;
+  longitude: number;
+  content: string;
+  image_url?: string;
+  location_name: string | null; // Gunakan null (bukan undefined)
+  users?: {
+    name: string;
+  };
+}
