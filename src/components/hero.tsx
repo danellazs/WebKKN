c17bbe0ef98c8adf84673f0d625966419347a7b5
danellazs <danella.zefanya@gmail.com>
2025-06-10 01:09:36 +0700

    add image landing hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,23 +1,19 @@
-"use client";
-
 import React from "react";
-import pantaiTemajuk from "../assets/pantai-temajuk.jpg";
+import pasir from "../assets/pasir.png";
+import ombak1 from "../assets/ombak1.png";
 
 const Hero: React.FC = () => {
   return (
-    <section
-      id="home"
-      className="hero-section"
-      style={{ backgroundImage: `url(${pantaiTemajuk})` }}
-    >
-      <div className="hero-overlay">
-        <div className="hero-text">
-          <h1 style={{ fontFamily: "BODAR" }}>
-            Menjejak Asa
-            <br />
-            di Ekor Borneo
-          </h1>
-        </div>
+    <section id="home" className="hero-section">
+      <img src={pasir} alt="Pantai Temajuk" className="hero-image" />
+      <img src={ombak1} alt="Ombak" className="ombak-image" />
+      <div className="hero-overlay" />
+      <div className="hero-text" style={{ fontFamily: "BODAR" }}>
+        <h1>
+          Menjejak Asa
+          <br />
+          di Ekor Borneo
+        </h1>
       </div>
     </section>
   );
